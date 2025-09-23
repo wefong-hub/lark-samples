be5255b50d7470ed727e7b9075dbcee32cd69126
wefong-hub <wefong@bytedance.com>
2025-09-23 04:44:24 +0000

    changing text into builder directly

--- echo_bot/python/main.py
+++ echo_bot/python/main.py
@@ -28,7 +28,7 @@ def do_p2_im_message_receive_v1(data: P2ImMessageReceiveV1) -> None:
                 CreateMessageRequestBody.builder()
                 .receive_id(data.event.message.chat_id)
                 .msg_type("text")
-                .content(content)
+                .content("Hello World")
                 .build()
             )
             .build()
